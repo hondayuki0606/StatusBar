3f76dd791a39855c567769952a6a9e6b1d394b74
honda <hondayuki0606@yahoo.co.jp>
2020-10-27 22:01:47 +0900

    initialize

--- StatusBar/AppDelegate.m
+++ StatusBar/AppDelegate.m
@@ -9,19 +9,34 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
+@property (weak) IBOutlet NSMenu *statusBar;
 
 @property (weak) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
-
+  NSStatusItem *_statusItem;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    
     // Insert code here to initialize your application
+    [self setupStatusItem];
+    // 次画面遷移処理を記載する
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+
+}
+
+- (void)setupStatusItem
+{
+    NSStatusBar *systemStatusBar = [NSStatusBar systemStatusBar];
+    _statusItem = [systemStatusBar statusItemWithLength:NSVariableStatusItemLength];
+    [_statusItem setHighlightMode:YES];
+    //[_statusItem setTitle:@"StatusBarApp"];
+    [_statusItem setImage:[NSImage imageNamed:@"StatusBarIconTemplate"]];
+    [_statusItem setMenu:self.statusBar];
 }
 
 
